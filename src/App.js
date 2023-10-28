7f56c971ffe1335ff8f41390a5306106cb5e8d07
Yuanke <yuankemiao.dev@gmail.com>
2023-10-28 18:37:07 +0300

    skeleton of page, components and scss files, colors, skeleton of AddNew components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.scss';
+import AddNew from './components/AddNew';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
-      HELLO WORLD
+      <h1>ToDo List</h1>
+      <p>This is a demo of todo list. After you refresh this page, all the data will be lost.</p>
+      <section className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </section>
+      <AddNew />
     </div>
   );
 }
